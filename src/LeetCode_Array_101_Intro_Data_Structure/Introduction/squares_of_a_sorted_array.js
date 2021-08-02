facca253890f5a478f6ac7dfbfda224a44c2e8d6
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-02 07:32:31 +0200

    Aufgabe: Squares of Sorted Array + Lösung hinzugefügt

---
+++ src/LeetCode_Array_101_Intro_Data_Structure/Introduction/squares_of_a_sorted_array.js
@@ -0,0 +1,25 @@
+/* Leetcode - Array 101: Squares of a Sorted Array */
+
+// Given an integer array nums sorted in non-decreasing order, 
+// return an array of the squares of each number sorted in non-decreasing order.
+
+/*
+    Input: nums = [-4,-1,0,3,10]
+    Output: [0,1,9,16,100]
+    Explanation: After squaring, the array becomes [16,1,0,9,100].
+    After sorting, it becomes [0,1,9,16,100].
+*/
+
+const sortedSquares = function(nums) {
+    let squaredNums = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        squaredNums.push(nums[i]**2);
+    }
+
+    return squaredNums.sort(function(a, b) {
+        return a - b;
+    });
+};
+
+sortedSquares([-4,-1,0,3,10]); // [0,1,9,16,100]
